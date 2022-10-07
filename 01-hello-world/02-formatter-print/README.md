5f935fdbfefed336cc273ac4c4f6dd87340afaa4
Larry Botha <lawrenceguy@gmail.com>
2022-10-07 00:32:43 -0500

    feat(formatted print): add notes and examples

--- 01-hello-world/02-formatter-print/README.md
+++ 01-hello-world/02-formatter-print/README.md
@@ -1,5 +1,66 @@
 # Formatted Print
 
-https://doc.rust-lang.org/stable/rust-by-example/hello/print.html
+- https://doc.rust-lang.org/stable/rust-by-example/hello/print.html
+- https://doc.rust-lang.org/std/fmt/
 
 ## Takeaways
+
+- Rust's `format!` macro is heavily inspired by, and similar to, Python's
+  `str.format`
+  - conventions for named parameters, indentation, and even keyword arguments
+    are familiar to those who work with Python
+- `format!` writes formatted text to a `String`
+- `print!` prints to stdout
+- `eprint!` prints to stderr
+- `:` in formatted strings indicates the start of the format spec - see
+  [syntax](https://doc.rust-lang.org/std/fmt/#syntax)
+  - everything after the `:` in a `{...}` format block describes the trait used
+    to interpet the block, the indentation, decimal precision, and
+    transformations
+- format characters after the `:` indicate the format to transform values to:
+  - `{}` - `Display` trait
+    - implementing `Display` for a struct is similar to implementing `__str__`
+      for a class in Python
+  - `{:?}` - `Debug` trait
+    - implementing or deriving `Debug` is akin to implementing `__repr__` in
+      Python
+  - `{:b}` - binary
+  - `{:o}` - octal
+  - `{:x}` - hexadecimal lowercase
+  - `{:X}` - hexadecimal uppercase
+  - `{:#formatter$}` - where `formatter$` is one of the above - uses the
+    formatter's alternate syntax to format the value
+- named arguments in the format spec require a `$` appended to them:
+
+  ```rust
+  let width = 5;
+
+  println!("{:width$}", 42); // => 00004
+  ```
+
+### Additional
+
+- `#[allow(dead_code)]` allows for defining structs that aren't used:
+
+  ```rust
+  #[allow(dead_code)]
+  struct UnusedStruct {}
+  ```
+
+- implementing `Display` automatically implements the `ToString` trait on the
+  type, adding the `to_string` method to instances:
+
+  ```rust
+  struct MyStruct {}
+
+  impl fmt::Display for MyStruct {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+      write!(f, "hello there", &self.bar)
+    }
+  }
+
+  let a = MyStruct {};
+  let a_string = a.to_string();
+
+  println!("{a}"); // => hello there
+  ```
--- 01-hello-world/02-formatter-print/src/main.rs
+++ 01-hello-world/02-formatter-print/src/main.rs
@@ -1,3 +1,109 @@
+use std::fmt;
+
+// allow for dead code to compile
+#[allow(dead_code)]
+struct UnusedStruct {}
+
+fn format_example() {
+    let a = format!("A dynamic value: {}", "foo");
+    println!("{}", a);
+
+    let a = format!("keyword arg: {value}", value = "foo");
+    println!("{}", a);
+
+    let value = "foo";
+    let a = format!("named arg: {value}");
+    println!("{}", a);
+
+    let a = format!("positioned args: {1} {0}", "first", "second");
+    println!("{}", a);
+
+    println!();
+}
+
+fn eprint_example() {
+    let a = "foo";
+    eprintln!("{}", a);
+}
+
+fn indentation() {
+    let a = "foo";
+    let x = 42;
+    let width = 10;
+
+    println!("occupy {width} using spaces right: |{:>width$}|", a);
+    println!("occupy {width} using spaces left: |{:<width$}|", a);
+    println!("occupy {width} using spaces centered: |{:^width$}|", a);
+    println!("occupy {width} using - centered: |{:-^width$}|", a);
+    println!(
+        "pad with leading zeros until width of {width}: |{:0width$}|",
+        x
+    );
+    println!(
+        "pad with leading zeros until width of {width}: |{:0width$}|",
+        -x
+    );
+    println!("decimal precision of {width}: |{:.width$}|", 5.0);
+    println!();
+}
+
+fn format_characters() {
+    #[derive(Debug)]
+    struct Foo {
+        bar: String,
+    }
+
+    impl fmt::Display for Foo {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "Here we have a Foo: {{ bar: {} }}", &self.bar)
+        }
+    }
+
+    let a = Foo {
+        bar: "baz".to_string(),
+    };
+    let x = 42;
+
+    println!("Display trait         {{}}: {}", a);
+    println!("Debug trait           {{:?}}: {:?}", a);
+    println!("binary {0}            {{:b}}: {0:b}", x);
+    println!("octal {0}             {{:o}}: {0:o}", x);
+    println!("hexadecimal lower {0} {{:x}}: {0:x}", x);
+    println!("hexadecimal upper {0} {{:X}}: {0:X}", x);
+    println!();
+
+    println!("alternate Display trait        {{:#}}: {:#}", a);
+    println!("alternate Debug trait          {{:#?}}: {:#?}", a);
+    println!("alternate binary {0}            {{:#b}}: {0:#b}", x);
+    println!("alternate octal {0}             {{:#o}}: {0:#o}", x);
+    println!("alternate hexadecimal lower {0} {{:#x}}: {0:#x}", x);
+    println!("alternate hexadecimal upper {0} {{:#X}}: {0:#X}", x);
+}
+
+fn impl_display_implements_to_string() {
+    struct Foo {
+        bar: String,
+    }
+
+    impl fmt::Display for Foo {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "Here we have a Foo: {{ bar: {} }}", &self.bar)
+        }
+    }
+
+    let a = Foo {
+        bar: "baz".to_string(),
+    };
+    // we didn't define .to_stirng() on Foo - it's automatially implemented
+    // when we implement Display
+    let foo_string = a.to_string();
+    println!("Foo.to_string: {}", foo_string);
+}
+
 fn main() {
-    println!("Hello, world!");
+    format_example();
+    eprint_example();
+    indentation();
+    format_characters();
+    impl_display_implements_to_string();
 }
