c7c8b63bf579bdaa12ac75128f3bb2d2bf08f271
Larry Botha <lawrenceguy@gmail.com>
2022-10-15 00:48:09 -0500

    feat(custom types): add notes and examples for structs

--- 03-custom-types/README.md
+++ 03-custom-types/README.md
@@ -3,3 +3,75 @@
 - https://doc.rust-lang.org/stable/rust-by-example/custom_types.html
 
 ## Takeaways
+
+- custom types in Rust are created primarily using enums and structs
+- constants can be created using the `const` and `static` keywords
+
+### structs
+
+- `structs` is short for _structures_
+- there are 3 main types of structs:
+  - tuple structs - these are basically named tuples - i.e. a tuple that is
+    defined by a type, _not_ like `namedtuple` in Python
+  - C structs, which are similar to interfaces in Typescript
+  - unit structs, which are useful when creating generic types. Unit structs
+    have no fields
+- accessing values in a tuple struct is done the same way as in normal tuples -
+  via 0-indexed keys
+- tuple structs may also be instantiated with keys:
+
+  ```rust
+  struct MyTuple(i32, i32, i32);
+
+  let my_tuple = MyTuple {0: 3, 1: 5, 2: 7};
+  ```
+
+- similar to objects in Javascript, structs can be instantiated with shorthand:
+
+  ```rust
+  struct Thing {
+    my_field: String,
+  };
+
+  let my_field = String::from("foo");
+  let thing = Thing { my_field }
+  ```
+
+- _struct update syntax_ is the name given to spreading values into other
+  structs:
+
+  ```rust
+  struct MyStruct {
+    a: i32,
+    b: i32,
+    c: i32,
+  };
+
+  let first = MyStruct { a: 1, b: 2, c: 3 };
+  let second = MyStruct { b: 3, ..first };
+  ```
+
+  _struct update syntax_ can only be used after other fields have been
+  specified
+
+- structs can be destructured in the same way they can in Javascript and Python,
+  except that that the name of the struct is required when destructuring:
+
+  ```rust
+  struct MyStruct {
+    foo: i32,
+    bar: i32,
+  };
+
+  let thing = MyStruct { foo: 1, bar: 2 };
+  let MyStruct { foo: not_foo, bar } = thing;
+  ```
+
+  Destructuring can be nested - see the `struct_exercise` function in
+  [./src/main.rs](./src/main.rs)
+
+### Additional
+
+- `#[allow(dead_code)]` will silence dead-code notifications
+- `assert_eq!` can be used to assert equality of two values, as syntactic sugar
+  for `assert!(a == b)`
--- 03-custom-types/src/main.rs
+++ 03-custom-types/src/main.rs
@@ -1,3 +1,163 @@
+fn tuple_structs() {
+    #[derive(Debug)]
+    struct MyTuple(i32, char, String);
+
+    let tup_1 = MyTuple(42, 'h', String::from("foo"));
+    println!("&tup_1: {:?}", &tup_1);
+    println!("tup_1.0: {}", tup_1.0);
+
+    let tup_2 = MyTuple {
+        2: String::from("foo"),
+        1: 'h',
+        0: 42,
+    };
+    println!("&tup_2: {:?}", &tup_2);
+    println!("tup_2.0: {}", tup_2.0);
+
+    println!();
+}
+
+fn c_structs() {
+    #[derive(Debug)]
+    #[allow(dead_code)]
+    struct Thing<'a> {
+        foo: String,
+        bar: &'a str,
+    }
+
+    let not_foo = "foo".to_string();
+    let bar = "hey";
+    let thing = Thing { foo: not_foo, bar };
+
+    println!("{:?}", &thing);
+    println!();
+}
+
+fn unit_structs() {
+    #[derive(Debug)]
+    struct MyUnit;
+
+    impl std::fmt::Display for MyUnit {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "aw I'm a unit: {:?}", &self)
+        }
+    }
+
+    let unit = MyUnit;
+    println!("{unit}");
+    println!();
+}
+
+fn struct_update_syntax() {
+    #[derive(Debug)]
+    #[allow(dead_code)]
+    struct Thing<T> {
+        a: T,
+        b: T,
+        c: T,
+    }
+
+    let thing_1 = Thing { a: 1, b: 2, c: 3 };
+    let thing_2 = Thing { b: 3, ..thing_1 };
+
+    println!("thing 1: {:?}", &thing_1);
+    println!("thing 2: {:?}", &thing_2);
+    assert!(thing_1.a == thing_2.a);
+    assert_eq!(thing_1.c, thing_2.c);
+    println!();
+}
+
+fn struct_destructuring() {
+    struct Thing<T> {
+        a: T,
+        b: T,
+        c: T,
+    }
+
+    let thing = Thing { a: 1, b: 2, c: 3 };
+    let Thing {
+        a: another_a,
+        b: hello,
+        c,
+    } = thing;
+
+    println!("another_a: {another_a}, hello: {hello}, c: {c}");
+    println!();
+}
+
+fn struct_exercise() {
+    #[derive(Debug)]
+    struct Point<T = f32>(T, T);
+
+    struct Rectangle<T = f32> {
+        top_left: Point<T>,
+        bottom_right: Point<T>,
+    }
+
+    impl std::fmt::Debug for Rectangle {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            let Rectangle {
+                top_left,
+                bottom_right,
+            } = &self;
+            let Point(x1, y1) = &top_left;
+            let Point(x2, y2) = &bottom_right;
+
+            write!(
+                f,
+                "
+              [{x1}, {y1}]
+                ---------------------
+                |                   |
+                |                   |
+                |                   |
+                |                   |
+                ---------------------
+                                   [{x2}, {y2}]
+            ",
+            )
+        }
+    }
+
+    impl Rectangle {
+        fn area(&self) -> f32 {
+            let Rectangle {
+                top_left: Point(x1, y1),
+                bottom_right: Point(x2, y2),
+            } = &self;
+
+            (x2 - x1) * (y2 - y1)
+        }
+
+        fn from(point: Point, scalar: f32) -> Rectangle {
+            let bottom_right = Point(point.0 + scalar, point.1 + scalar);
+            Rectangle {
+                top_left: point,
+                bottom_right,
+            }
+        }
+    }
+
+    let top_left = Point(1.0, 2.3);
+    let rect = Rectangle {
+        top_left,
+        bottom_right: Point(3.0, 5.2),
+    };
+    let square = Rectangle::from(Point(1.0, 4.4), 3.0);
+
+    println!("rect: {:?}", &rect);
+    println!("rect area: {}", rect.area());
+
+    println!("square: {:?}", &square);
+    println!("square area: {}", square.area());
+}
+
 fn main() {
-    println!("Hello, world!");
+    // structs
+    tuple_structs();
+    c_structs();
+    unit_structs();
+    struct_update_syntax();
+    struct_destructuring();
+    struct_exercise();
 }
