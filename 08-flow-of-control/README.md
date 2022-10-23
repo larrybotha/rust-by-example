be4359486f891203deffe9eee19539e118c4df1b
Larry Botha <lawrenceguy@gmail.com>
2022-10-23 01:16:43 -0500

    feat(flow of control): add notes and examples for loop, while, and for in

--- 08-flow-of-control/README.md
+++ 08-flow-of-control/README.md
@@ -1,4 +1,120 @@
-# Flow Control
+# Flow of Control
 
 - https://doc.rust-lang.org/stable/rust-by-example/flow_control.html
 - https://doc.rust-lang.org/stable/book/ch03-05-control-flow.html
+
+## Takeaways
+
+### if / else
+
+- conditions in if / else statements in Rust don't need to be contained in
+  parens, similar to Python, and different to Javascript
+- if / else conditionals are expressions - they can evaluate to a result, and
+  thus be used for assignment to variables. This mitigates the hell of nested
+  ternaries in Javascript:
+
+  ```rust
+  let x = 5;
+  let result = if x >= 0 && x < 1 {
+      "foo"
+  } else if x >= 1 && x < 2  {
+      "bar"
+  } else if x >= 2 && x < 3 {
+      "baz"
+  } else {
+      "quux"
+  };
+  ```
+
+- all branches in if / else conditionals must evaluate to the same type
+
+### `loop`
+
+- `loop` allows for looping infinitely in Rust - it is equivalent to `while true`
+- as in other languages, `break` will exit a loop, and `continue` will move
+  execution to the next iteration
+
+#### Nesting and labels
+
+- as in Javascript, one can label loops to specify which loop should be affected
+  when breaking or continuing:
+
+  ```rust
+  let mut count_outer = 0;
+
+  'outer: loop {
+      count_outer += 1;
+
+      let mut count_inner = 0;
+
+      'inner: loop {
+          count_inner += 1;
+
+          if count_outer % 2 == 0 {
+              continue 'outer;
+          }
+
+          if count_inner >= 3 {
+              break 'inner;
+          }
+      }
+
+      if count_outer >= 10 {
+          break 'outer;
+      }
+  }
+  ```
+
+#### Returning from loops
+
+- as with if / else conditionals, a loop can be used to resolve to a value, and
+  thus for variable assignment, by providing a value after `break`:
+
+  ```rust
+  let count = 0;
+  let result = loop {
+    count += 1;
+
+    if count > 5 {
+        break count;
+    };
+  };
+  ```
+
+### `while`
+
+- `while` in Rust is similar to other languages, with the same syntax as `loop`
+
+### `for` loops
+
+- the `for in` syntax, similar to Python's, is used to iterate over `Iterator`s
+- a range is an `Iterator`:
+
+  ```rust
+  // 0,1,2,3,4,5
+  for x in 0..6 {
+      // ...
+  }
+  ```
+
+- collections, such as arrays, slices, and vectors, can be converted to
+  `Iterator`s using a few methods:
+  - `.into_iter` - will move ownership of each value into the loop,
+    invalidating the original iterated value
+  - `.iter` - will process each item in the iterator as a borrowed value,
+    leaving the original collection intact
+  - `.iter_mut` - will process each item in the iterator as a mutable borrowed
+    value, allowing for mutating the original collection
+- by default, `for in` uses `.into_iter` on collections
+
+### Additional
+
+- `String.is_empty()` is syntactic sugare for `String.len() > 0`
+- concatenating `String`s is not as simple as adding them together - a `String`
+  must be followed by `&str` values
+- ternary operators in Rust are not like ternaries in Python or Javascript:
+
+  ```rust
+  let x = 5;
+  let y = if x > 0 { true } else { false };
+  ```
--- 08-flow-of-control/src/main.rs
+++ 08-flow-of-control/src/main.rs
@@ -1,3 +1,200 @@
+fn if_else_no_parens() {
+    let x = 5;
+
+    if x > 0 {
+        println!("greater than 0");
+    } else {
+        println!("not greater than 0");
+    }
+
+    println!();
+}
+
+fn if_else_expressions() {
+    let x = 5;
+    let result = if x > 0 && x < 5 {
+        "less than"
+    } else if x == 5 {
+        "equal to"
+    } else {
+        "greater than"
+    };
+
+    println!("x is '{result}' 5");
+    println!();
+}
+
+fn loop_example() {
+    let mut count = 0;
+    let mut xs: Vec<i32> = vec![];
+
+    loop {
+        count += 1;
+
+        if count % 2 != 0 {
+            println!("skipping odd number");
+            // continue to next iteration
+            continue;
+        }
+
+        if count >= 10 {
+            println!("breaking at {count}");
+            // break out of the loop
+            break;
+        }
+
+        xs.push(count);
+    }
+
+    println!("xs: {xs:?}");
+    println!();
+}
+
+fn nested_loops_and_labels() {
+    let mut count = 0;
+    let mut xs: Vec<Vec<i32>> = vec![];
+
+    'loop_outer: loop {
+        count += 1;
+        let mut zs: Vec<i32> = vec![];
+        let mut inner_count = 0;
+
+        'loop_inner: loop {
+            inner_count += 1;
+
+            if zs.iter().sum::<i32>() > 10 {
+                break 'loop_inner;
+            }
+
+            if inner_count == 5 {
+                println!("continuing 'loop_outer from 'loop_inner");
+                continue 'loop_outer;
+            }
+
+            zs.push(count + inner_count);
+        }
+
+        xs.push(zs);
+
+        if count >= 5 {
+            break 'loop_outer;
+        }
+    }
+
+    println!("xs: {xs:#?}");
+    println!();
+}
+
+fn return_from_loop() {
+    let mut count = 0;
+    let result = loop {
+        count += 1;
+
+        if count >= 5 {
+            break count;
+        };
+    };
+
+    assert_eq!(result, 5);
+    println!("result: {result}");
+    println!();
+}
+
+fn while_example() {
+    let mut count = 0;
+
+    while count < 5 {
+        println!("count is {count}");
+
+        count += 1;
+
+        if count >= 5 {
+            break;
+        }
+    }
+
+    println!();
+}
+
+fn for_example() {
+    // a range from 0 to 31 exclusive
+    for x in 0..31 {
+        // ternary's require blocks to evaluate to
+        let fizz = if x % 3 == 0 { "fizz" } else { "" };
+        let buzz = if x % 5 == 0 { "buzz" } else { "" };
+        let xs = vec![fizz, buzz]
+            .iter()
+            .filter(|z| !z.is_empty()) // the type here is &&&str... blegh..?
+            .fold(String::new(), |acc, s| acc + s);
+
+        if !xs.is_empty() {
+            println!("{x:>2}: {xs}");
+        }
+    }
+
+    println!();
+}
+
+fn for_into_iter_mut() {
+    let xs = vec![1, 2, 3]; // get a heap allocated value
+
+    println!("for loop, implicit .into_iter");
+    // implicitlry uses xs.into_iter, invalidating xs
+    for x in xs {
+        println!("x: {x}");
+    }
+    println!();
+
+    // the following will not compile - xs is invalid here
+    //println!("{xs:?}");
+
+    let xs = vec![1, 2, 3];
+
+    println!("for loop, explicit .into_iter");
+    // same as previous loop
+    for x in xs.into_iter() {
+        println!("x: {x}");
+    }
+    println!();
+
+    // will also not compile
+    //println!("{xs:?}");
+
+    let xs = vec![1, 2, 3];
+
+    println!("for loop, explicit .into_iter");
+    // same as previous loop
+    for x in xs.iter() {
+        println!("x: {x}");
+    }
+
+    println!("xs is still valid: {xs:?}");
+    println!();
+
+    let mut xs = vec![1, 2, 3];
+
+    for x in xs.iter_mut() {
+        println!("squaring {x}");
+        //*x = *x * *x; // we need to dereference x before mutating it...
+        // for some reason
+        let squared = *x * *x;
+        *x = squared;
+    }
+
+    println!("xs is valid and mutated: {xs:?}");
+    println!();
+}
+
 fn main() {
-    println!("Hello, world!");
+    if_else_no_parens();
+    if_else_expressions();
+
+    loop_example();
+    nested_loops_and_labels();
+    return_from_loop();
+
+    while_example();
+
+    for_example();
+    for_into_iter_mut();
 }
