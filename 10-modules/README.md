525724f61391e2dc8577342289e779433cc328ee
Larry Botha <lawrenceguy@gmail.com>
2022-11-10 10:11:09 -0500

    feat(modules): add notes on visibility in modules

--- 10-modules/README.md
+++ 10-modules/README.md
@@ -3,3 +3,19 @@
 - https://doc.rust-lang.org/stable/rust-by-example/mod.html
 
 ## Takeaways
+
+- a module is a collection of functions, structs, traits, `impl` blocks, and
+  other modules
+
+### Visibility
+
+- modules are declared using the `mod` keyword
+- as in Javascript, and in opposition to Python, everything in a module is
+  private by default
+- the `pub` keyword allows one to make an item public
+- items in nested modules can be made available to the parent and other modules
+  in a number of ways:
+  - using `pub(self)` - no access outside of the module, the same as _not_
+    using `pub`
+  - using `pub(in crate::path)` - accessible to any other module in the crate
+  - using `pub(super)` - accessible to the parent of the current module
--- 10-modules/src/main.rs
+++ 10-modules/src/main.rs
@@ -1,3 +1,88 @@
+mod my_mod {
+    pub fn public_function() {
+        println!("called my_mod::public_function")
+    }
+
+    fn private_function() {
+        println!("called my_mod::private_function")
+    }
+
+    pub fn indirect_access() {
+        println!("called my_mod::indirect_access");
+        println!("calling my_mod::private_function...");
+        private_function()
+    }
+
+    pub fn nested_a_parent_access() {
+        println!("called my_mod::nested_a_parent_access");
+        println!("calling my_mod::nested_a::nested_available_in_parent");
+        nested_a::nested_available_in_parent();
+        println!("called my_mod::nested_a::nested_available_in_parent");
+    }
+
+    pub fn nested_a_crate_access() {
+        println!("called my_mod::nested_a_parent_access");
+        println!("calling my_mod::nested_a::nested_available_in_crate");
+        nested_a::nested_available_in_crate();
+        println!("called my_mod::nested_a::nested_available_in_crate");
+    }
+
+    pub mod nested_a {
+        // This function is only accessible inside my_mod::nested_a.
+        // The pub(self) syntax is equivalent to not using pub at all
+        #[allow(dead_code)]
+        pub(self) fn nested_private_explicit() {
+            println!("called my_mod::nested_a::nested_private_explicit")
+        }
+
+        pub fn nested_public_function() {
+            println!("called my_mod::nested_a::public_function")
+        }
+
+        pub fn nested_indirect_access() {
+            println!("called my_mod::nested::nested_indirect_access");
+            println!("calling my_mod::nested_a::nested_private_explicit...");
+            nested_private_explicit()
+        }
+
+        pub(in crate::my_mod) fn nested_available_in_crate() {
+            println!("called my_mod::nested_a::nested_available_in_crate")
+        }
+
+        pub(super) fn nested_available_in_parent() {
+            println!("called my_mod::nested_a::nested_available_in_crate")
+        }
+    }
+
+    pub mod nested_b {
+        pub fn nested_a_crate_access() {
+            println!("called my_mod::nested_b::nested_a_parent_access");
+            println!("calling crate::my_mod::nested_a::nested_available_in_crate");
+            crate::my_mod::nested_a::nested_available_in_crate();
+            println!("called crate::my_mod::nested_a::nested_available_in_crate");
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    my_mod::public_function();
+    println!();
+
+    my_mod::indirect_access();
+    println!();
+
+    my_mod::nested_a::nested_public_function();
+    println!();
+
+    my_mod::nested_a::nested_indirect_access();
+    println!();
+
+    my_mod::nested_a_parent_access();
+    println!();
+
+    my_mod::nested_a_crate_access();
+    println!();
+
+    my_mod::nested_b::nested_a_crate_access();
+    println!();
 }
