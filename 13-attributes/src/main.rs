0604d85b2fae4b62342062061c52a32c7b611c15
Larry Botha <lawrenceguy@gmail.com>
2022-11-17 23:25:20 +0200

    feat(attributes): add notes and examples of attributes

--- 13-attributes/src/main.rs
+++ 13-attributes/src/main.rs
@@ -1,3 +1,22 @@
+// will be compile into binary / library only on MacOS
+#[cfg(target_os = "macos")]
+fn which_os() {
+    println!("You're on a mac!")
+}
+
+// will be compile into binary / library only on Linux
+#[cfg(target_os = "linux")]
+fn which_os() {
+    println!("You're on linux!")
+}
+
 fn main() {
-    println!("Hello, world!");
+    which_os();
+
+    // cfg! only ever evaluates to true or false, and does so at runtime
+    if cfg!(target_os = "macos") {
+        println!("You're on a mac!")
+    } else if cfg!(target_os = "linux") {
+        println!("You're on linux!")
+    }
 }
