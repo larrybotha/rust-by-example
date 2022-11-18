ec447de9f85f7277cecdeacab7ba40491c2c2701
Larry Botha <lawrenceguy@gmail.com>
2022-11-18 23:31:18 +0200

    feat(generics): add notes on generic structs and functions

--- 14-generics/README.md
+++ 14-generics/README.md
@@ -3,3 +3,29 @@
 - https://doc.rust-lang.org/stable/rust-by-example/generics.html
 
 ## Takeaways
+
+- generics are the mechanism for generalising types and functionalities
+- similarly to TypeScript, generic type parameters are defined in angled
+  brackets:
+
+  ```rust
+  fn some_func<T>(x: T) { ... }
+  ```
+
+- any type that is not generic is considered _concrete_
+
+## Functions
+
+- generic function parameters may be specificed implicitly, by the type passed
+  in, or explicitly when called:
+
+  ```rust
+  struct GenStruct<T>(T);
+
+  fn generic<T>(x: GenStruct<T>) { ... }
+
+  let my_gen_value = GenStruct("foo".to_string());
+
+  generic(my_gen_value); // implicitly specified
+  generic::<i8>(GenStruct(4)); // explicitly specified
+  ```
--- 14-generics/src/main.rs
+++ 14-generics/src/main.rs
@@ -1,3 +1,53 @@
+use std::any::type_name;
+
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}
+
+fn generic_structs() {
+    // concrete
+    struct A;
+    // concrete
+    struct Single(A);
+    // generic
+    struct SingleGeneric<T>(T);
+
+    let a = A;
+    let single_a = Single(A);
+    let single_generic_a = SingleGeneric(A);
+    let single_nested_single_a = SingleGeneric(Single);
+    let single_generic_i8: SingleGeneric<i8> = SingleGeneric(4);
+
+    println!("type of 'a' is:\n\t{:>10}\n", type_of(a));
+    println!("type of 'single_a' is:\n\t{:>10}\n", type_of(single_a));
+    println!(
+        "type of 'single_generic_a' is:\n\t{:>10}\n",
+        type_of(single_generic_a)
+    );
+    println!(
+        "type of 'single_nested_single_a' is:\n\t{:>10}\n",
+        type_of(single_nested_single_a)
+    );
+    println!(
+        "type of 'single_generic_i8' is:\n\t{:>10}\n",
+        type_of(single_generic_i8)
+    );
+    println!();
+}
+
+fn generic_functions() {
+    struct GenStruct<T>(T);
+
+    fn generic<T>(x: GenStruct<T>) {
+        println!("{}", type_of(x));
+    }
+
+    generic(GenStruct(5)); // implicitly specified type parameter -> i32
+    generic::<i8>(GenStruct(5)); // explicitly specified type parameter -> i8
+    println!();
+}
+
 fn main() {
-    println!("Hello, world!");
+    generic_structs();
+    generic_functions();
 }
