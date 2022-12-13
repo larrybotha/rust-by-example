4c7331afc2940737df2f75bd2f0a5c20fbb5ab58
Larry Botha <lawrenceguy@gmail.com>
2022-12-13 09:43:45 +0200

    feat(scoping rules): add notes and examples of RAII

--- 15-scoping-rules/README.md
+++ 15-scoping-rules/README.md
@@ -3,3 +3,39 @@
 - https://doc.rust-lang.org/stable/rust-by-example/scope.html
 
 ## Takeaways
+
+- scoping rules define how borrowing and lifetimes of values work
+- the compiler uses scoping rules to determine
+  - the validity of borrows
+  - when memory can be freed
+  - when variables are created and destroyed
+
+### RAII
+
+- RAII means _resource allocation is initialisation_ - when a value goes out of
+  scope, it is destroyed so that memory can be freed
+- variables in Rust don't only hold data - they _own_ resources, e.g. `Box` owns
+  memory in the heap
+- _owned_ values are stored on the heap, and these resources must be freed once
+  they are no longer needed so that other resources may have access to that
+  memory
+- when values go out of scope, their destructors are called
+- `Drop` is the trait used to create destructors. Most types in Rust
+  automatically implement `Drop`, but it may be implemented if custom drop
+  logic is required:
+
+  ```rust
+  struct DropStruct {}
+
+  impl Drop for DropStruct {
+    fn drop(&mut self) {
+      // do something before memory is freed
+    }
+  }
+  ```
+
+### Ownership and moves
+
+## Additional notes
+
+- [valgrid](https://valgrind.org/) is useful for profiling memory leaks on Linux
--- 15-scoping-rules/src/main.rs
+++ 15-scoping-rules/src/main.rs
@@ -1,3 +1,57 @@
+fn raii_example() {
+    fn create_box(value: i32) {
+        // _box is an owned value - the i32 is value is stored on the heap
+        println!("create_box allocating memory...");
+        let _box = Box::new(value);
+
+        println!("create_box freeing memory...")
+        // _box goes out of scope here, and memory is freed
+    }
+
+    // allocating an i32 to the heap
+    println!("_box_1 coming into scope");
+    let _box_1 = Box::new(5);
+
+    {
+        // allocating another i32 to the heap
+        println!("_box_2 coming into scope");
+        let _box_2 = Box::new(6);
+
+        println!("_box_2 going out of scope");
+        // _box_2 goes out of scope here, is destroyed, and memory is freed
+    }
+
+    // creating many boxes - no need to free memory
+    for x in 0i32..10 {
+        create_box(x);
+    }
+
+    println!("_box_1 going out of scope");
+    println!();
+    // _box_1 goes out of scope here, is destroyed, and memory is freed
+}
+
+fn custom_drop() {
+    use std::mem;
+
+    struct MyDropStruct(i32);
+
+    // custom drop logic
+    impl Drop for MyDropStruct {
+        fn drop(&mut self) {
+            println!("value uses {} bytes of memory", mem::size_of_val(self));
+            println!("we're dropping... weeeeee!!!")
+        }
+    }
+
+    {
+        let _value = MyDropStruct(42);
+    }
+
+    println!()
+}
+
 fn main() {
-    println!("Hello, world!");
+    raii_example();
+    custom_drop();
 }
