07d1d6cc8ca1ed4eb1607c8869b7dd0e0ff30132
Larry Botha <lawrenceguy@gmail.com>
2022-12-23 11:43:24 +0200

    feat(traits): add example for internal access to trait methods

--- 16-traits/README.md
+++ 16-traits/README.md
@@ -3,3 +3,6 @@
 - https://doc.rust-lang.org/stable/rust-by-example/trait.html
 
 ## Takeaways
+
+- traits are collections of methods defined for an unknown type, `Self`
+- methods within a trait have access to other methods within the trait
--- 16-traits/src/main.rs
+++ 16-traits/src/main.rs
@@ -1,3 +1,31 @@
+fn internal_access() {
+    trait MyTrait {
+        fn method_a(&self) -> &Self {
+            println!("method_a called!");
+            println!("calling self.method_b...");
+
+            self.method_b();
+
+            self
+        }
+
+        fn method_b(&self) -> &Self {
+            println!("method_b called!");
+
+            self
+        }
+    }
+
+    struct MyStruct {}
+
+    impl MyTrait for MyStruct {}
+
+    let x = MyStruct {};
+
+    x.method_a();
+    println!();
+}
+
 fn main() {
-    println!("Hello, world!");
+    internal_access();
 }
