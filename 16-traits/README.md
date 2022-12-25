76a1476bc81cb5505193dae17ad915e4bec3d6f3
Larry Botha <lawrenceguy@gmail.com>
2022-12-25 18:25:21 +0200

    feat(traits): add notes and examples for deriving, and using dyn

--- 16-traits/README.md
+++ 16-traits/README.md
@@ -6,3 +6,126 @@
 
 - traits are collections of methods defined for an unknown type, `Self`
 - methods within a trait have access to other methods within the trait
+- an instance of a struct that implements a trait may be instantiated via the
+  trait and a type annotation:
+
+  ```rust
+  trait MyTrait {
+    fn new() -> Self;
+  }
+
+  struct MyStruct {}
+
+  impl MyTrait for MyStruct {
+    fn new() -> Self {
+        MyStruct {}
+    }
+  }
+
+  let instance: MyStruct = MyTrait::new();
+  ```
+
+### Deriving traits
+
+- as in Haskell, there are traits that can be derived for types without
+  additional work
+- these derivable traits may also be manually implemented if need be
+- `Clone` is derivable, and will create a cloned instance of a type given a
+  reference to the original instance
+- `Copy` replaces a type's _copy semantics_ with _move semantics_
+- `Default` adds a `::default` associated function which returns an empty
+  instance of a type by default, which may also be useful to override:
+
+  ```rust
+  #[derive(Debug)]
+  struct Point {
+      x: i32,
+      y: i32
+  }
+
+  impl std::default::Default for Point {
+      fn default() -> Self {
+          Self { x: 0, y: 0 }
+      }
+  }
+  ```
+
+- as in Haskell, evaluating equality of types requires implementing traits
+  associated with equality:
+
+  ```rust
+  #[derive(Debug, PartialEq)]
+  struct Int(i32);
+
+  let x = Int(5);
+  let y = Int(6);
+
+  assert_ne!(x, y);
+  assert!(x.eq(&y));
+  ```
+
+  Deriving `PartialEq` adds `.eq` and `.ne` methods to the instance
+
+- `Eq` may only be applied to types that implement `PartialEq`, and where every
+  value of the type is equal to itself. An example of where this fails is for
+  floating point numbers, where `NaN` does not equal itself
+
+- as in Haskell, implementing `Ord`-like traits is required to compare values in
+  an ordinal fashion. Equality
+
+  ```rust
+    use std::cmp::Ordering;
+
+    #[derive(Debug, PartialEq, PartialOrd)]
+    struct Int(i32);
+
+    let x = Int(6);
+    let y = Int(5);
+
+    assert!(x > y);
+    assert!(x.gt(&y));
+    assert_eq!(x.cmp(&y), Ordering::Greater);
+  ```
+
+  `PartialOrd` adds `.gt`, `.lt`, `.le`, `.ge`, and `.cmp` methods to the instance.
+  `.cmp` returns a variant from the `std::cmp::Ordering` enum
+
+### Returning traits with `dyn`
+
+- the [Rust docs on trait objects](http://doc.rust-lang.org/1.65.0/reference/types/trait-object.html)
+  has a succinct example on using `dyn` to return a trait when the concrete
+  type is not known
+- generally, Rust requires a concrete type to be returned from functions, as
+  Rust requires at compile time a way to determine the memory allocation of a
+  type. Using `dyn` along with `Box` allows for an escape hatch, where one can
+  specify that something that is heap allocated and implements a specific
+  trait is going to be returned
+
+## Additional
+
+- functions in traits are all called associated functions - they're associated
+  with the type after `impl`
+
+  - associated functions may or may not be methods. Functions that have a
+    reference to the instance, as in other languages, are methods. Functions
+    that don't have a reference to the instance don't appear to be given a
+    special name, but are akin to static methods in other languages:
+
+    ```rust
+    trait MyTrait {
+        fn my_static_method();
+    }
+    ```
+
+    ```python
+    from abc.collections import ABCMeta
+
+    class MyBaseClass {
+        @staticmethod
+        def my_static_method():
+            pass
+    }
+    ```
+
+- prefer `const` over `static` for constants: [Static
+  items](http://doc.rust-lang.org/1.65.0/reference/items/static-items.html#using-statics-or-consts)
--- 16-traits/src/main.rs
+++ 16-traits/src/main.rs
@@ -1,3 +1,6 @@
+use rand::random;
+use std::default::Default;
+
 fn internal_access() {
     trait MyTrait {
         fn method_a(&self) -> &Self {
@@ -26,6 +29,223 @@ fn internal_access() {
     println!();
 }
 
+fn instantiate_via_trait() {
+    trait Animal {
+        fn new(name: &'static str) -> Self;
+        fn name(&self) -> &'static str;
+        fn noise(&self) -> &'static str;
+
+        fn talk(&self) {
+            println!("{} says {}", self.name(), self.noise())
+        }
+    }
+
+    struct Sheep {
+        name: &'static str,
+        naked: bool,
+    }
+
+    impl Sheep {
+        fn is_naked(&self) -> bool {
+            self.naked
+        }
+
+        fn shear(&mut self) -> &mut Self {
+            self.naked = if !self.naked { true } else { self.naked };
+
+            self
+        }
+    }
+
+    impl Animal for Sheep {
+        fn new(name: &'static str) -> Self {
+            Self { name, naked: false }
+        }
+
+        fn name(&self) -> &'static str {
+            self.name
+        }
+
+        fn noise(&self) -> &'static str {
+            if self.naked {
+                "baaaa!!!"
+            } else {
+                "baaaa?"
+            }
+        }
+    }
+
+    // we can instantiate via a trait by annotating the type
+    let mut dolly: Sheep = Animal::new("Dolly");
+
+    println!("is {} naked? {}", dolly.name(), dolly.is_naked());
+    dolly.talk();
+    println!("let's shave {}...", dolly.name());
+
+    dolly.shear();
+
+    println!("is {} naked now? {}", dolly.name(), dolly.is_naked());
+    dolly.talk();
+    println!();
+}
+
+fn default_trait() {
+    #[derive(Debug)]
+    #[allow(dead_code)]
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    impl Default for Point {
+        fn default() -> Self {
+            Self { x: 1, y: 1 }
+        }
+    }
+
+    let point = Point::default();
+
+    println!("point: {point:?}");
+    println!()
+}
+
+#[allow(clippy::eq_op, clippy::cmp_nan)]
+fn nan_never_equal() {
+    const NAN: f64 = f64::NAN;
+
+    println!("f64::NAN == f64::NAN: {}", NAN == NAN);
+    println!()
+}
+
+fn derive_equality_comparison() {
+    // Eq can be derived, as there are no values in i32
+    // that are not equal to themselves
+    #[derive(Debug, PartialEq, Eq)]
+    struct Integer(i32);
+
+    // Eq cannot be derived for f64 - NAN != NAN ever
+    #[derive(Debug, PartialEq)]
+    struct Float(f64);
+
+    let x = Integer(5);
+    let y = Integer(6);
+    let z = Integer(5);
+
+    assert_ne!(x, y);
+    assert_eq!(x, z);
+    println!("x.eq(&y): {}", x.eq(&y));
+    println!("x == y: {}", x == y);
+    println!()
+}
+
+fn derive_ordinal_comparison() {
+    #[derive(Debug, PartialEq, PartialOrd)]
+    struct Metre(f64);
+
+    use std::cmp::Ordering;
+
+    let x = 7;
+    let y = 6;
+
+    assert!(x > y);
+    assert_eq!(x.cmp(&y), Ordering::Greater);
+    assert!(x.gt(&y));
+    println!("x.cmp(&y): {:?} == {:?}", x.cmp(&y), Ordering::Greater);
+    println!("x.gt(&y): {}", x.gt(&y));
+    println!("x > y: {}", x > y);
+    println!();
+}
+
+fn dynamic_trait_no_struct() {
+    trait Printable {
+        fn stringify(&self) -> String;
+    }
+
+    // implement Printable for i32
+    impl Printable for i32 {
+        fn stringify(&self) -> String {
+            self.to_string()
+        }
+    }
+
+    fn print_using_bound<T: Printable>(x: T) {
+        println!("printing x using trait object: {}", x.stringify())
+    }
+
+    // we don't know what the concrete type for x is, but we do know that
+    // it must implement Printable
+    fn print_using_trait_object(x: Box<dyn Printable>) {
+        println!("printing x using trait object: {}", x.stringify())
+    }
+
+    print_using_bound(6);
+    print_using_trait_object(Box::new(6));
+
+    println!();
+}
+
+fn return_trait_with_dyn() {
+    //use rand;
+
+    trait Animal {
+        fn noise(&self) -> &'static str;
+        fn name(&self) -> &'static str;
+    }
+
+    #[derive(Debug)]
+    struct Cow {}
+
+    impl Animal for Cow {
+        fn noise(&self) -> &'static str {
+            "mooooooooooooo"
+        }
+
+        fn name(&self) -> &'static str {
+            "Cow"
+        }
+    }
+
+    #[derive(Debug)]
+    struct AnglerFish {}
+
+    impl Animal for AnglerFish {
+        fn noise(&self) -> &'static str {
+            "bloop bloop"
+        }
+
+        fn name(&self) -> &'static str {
+            "Angler Fish"
+        }
+    }
+
+    fn random_animal(x: f64) -> Box<dyn Animal> {
+        if x > 0.5 {
+            Box::new(Cow {})
+        } else {
+            Box::new(AnglerFish {})
+        }
+    }
+
+    let rand_float = random();
+    let animal = random_animal(rand_float);
+
+    println!("random value is {}", rand_float);
+    println!("{} goes {}", animal.name(), animal.noise());
+
+    println!();
+}
+
 fn main() {
     internal_access();
+    instantiate_via_trait();
+
+    // deriving
+    default_trait();
+    nan_never_equal();
+    derive_equality_comparison();
+    derive_ordinal_comparison();
+
+    // dyn
+    dynamic_trait_no_struct();
+    return_trait_with_dyn();
 }
