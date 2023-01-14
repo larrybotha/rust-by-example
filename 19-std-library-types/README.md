3648502010500428c911e30e11c7a547731b55de
Larry Botha <lawrenceguy@gmail.com>
2023-01-14 20:19:24 +0200

    feat(std library types): add notes on Box

--- 19-std-library-types/README.md
+++ 19-std-library-types/README.md
@@ -1,5 +1,34 @@
 # Std library types
 
 - https://doc.rust-lang.org/stable/rust-by-example/std.html
+- https://doc.rust-lang.org/std/
 
 ## Takeaways
+
+- in addition to primitives, i.e. scalar types and compound types, Rust's `std`
+  library provides many custom types which expand on the primitives
+
+### `Box`, stack, and heap
+
+- in Rust, all values are stack-allocated by default
+- `Box` allows one to heap-allocate a value
+- when a `Box`d value goes out of scope:
+  - the `Box`s destructor is called
+  - the internal value is destroyed
+  - the memory on the heap is freed
+- `Box<T>` is a smart pointer to a heap-allocated value `T`
+- a `Box`d value can be dereferenced using `*`, which removes only 1 layer of
+  indirection
+
+## Additional
+
+- to determine the number of bytes a value occupies on the stack, one can use
+  `std::mem::size_of_val(x)`:
+
+  ```rust
+  use std::mem;
+
+  let x = 5;
+
+  println!("x occupies {} bytes on the stack", mem::size_of_val(&x));
+  ```
--- 19-std-library-types/src/main.rs
+++ 19-std-library-types/src/main.rs
@@ -1,3 +1,74 @@
+fn boxed_values() {
+    use std::mem;
+
+    #[derive(Debug)]
+    #[allow(dead_code)]
+    struct Point {
+        x: f64,
+        y: f64,
+    }
+
+    impl Point {
+        fn new(x: f64, y: f64) -> Self {
+            Self { x, y }
+        }
+    }
+
+    impl std::default::Default for Point {
+        fn default() -> Self {
+            Self { x: 0.0, y: 0.0 }
+        }
+    }
+
+    #[derive(Debug)]
+    #[allow(dead_code)]
+    struct Rectangle {
+        top_left: Point,
+        bottom_right: Point,
+    }
+
+    fn origin() -> Point {
+        Point::default()
+    }
+
+    fn boxed_origin() -> Box<Point> {
+        #[allow(clippy::box_default)]
+        Box::new(Point::default())
+    }
+
+    fn debug_stack(name: &str, x: &impl std::fmt::Debug) {
+        println!("{name}: {x:?}");
+        println!(
+            "{name} occupies {} bytes on the stack\n",
+            mem::size_of_val(x)
+        )
+    }
+
+    let point = origin();
+    let double_boxed_point: Box<Box<Point>> = Box::new(boxed_origin());
+    let rectangle = Rectangle {
+        top_left: origin(),
+        // we can convert i32 into f64 using .into() -
+        // .into() determines the resulting type depending on context
+        bottom_right: Point::new(3.into(), (-4).into()),
+    };
+    let boxed_rectangle: Box<Rectangle> = Box::new(Rectangle {
+        top_left: origin(),
+        bottom_right: Point::new(3.0, -4.0),
+    });
+
+    debug_stack("point", &point);
+    debug_stack("double_boxed_point", &double_boxed_point);
+    debug_stack("*double_boxed_point", &*double_boxed_point);
+    debug_stack("**double_boxed_point", &**double_boxed_point);
+    println!();
+
+    debug_stack("rectangle", &rectangle);
+    debug_stack("boxed_rectangle", &boxed_rectangle);
+    debug_stack("*boxed_rectangle", &*boxed_rectangle);
+    println!()
+}
+
 fn main() {
-    println!("Hello, world!");
+    boxed_values();
 }
