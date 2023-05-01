236d3446c214caaf966d3db296db5fefb6990e77
Larry Botha <lawrenceguy@gmail.com>
2023-04-30 22:46:18 -0500

    feat(std misc): add notes and examples for threads

--- 20-std-misc/src/main.rs
+++ 20-std-misc/src/main.rs
@@ -1,3 +1,34 @@
+fn thread_example() {
+    use std::thread;
+    use std::thread::JoinHandle;
+
+    const NUM_THREADS: u32 = 10;
+
+    #[allow(clippy::needless_collect)]
+    let children = (0..NUM_THREADS)
+        .map(|i| {
+            thread::spawn(move || {
+                println!("thread executing for iteration {}", &i);
+
+                format!("result for this thread: {i}")
+            })
+        })
+        .collect::<Vec<JoinHandle<String>>>();
+
+    println!("\nnow waiting for each thread to complete...\n");
+
+    children
+        .into_iter()
+        .map(|child| child.join()) // wait for the thread to complete
+        // or
+        //.map(JoinHandle::join)
+        .map(|result| println!("{result:?}"))
+        .for_each(drop);
+
+    println!()
+}
+
+// `main` is the main thread...
 fn main() {
-    println!("Hello, world!");
+    thread_example();
 }
